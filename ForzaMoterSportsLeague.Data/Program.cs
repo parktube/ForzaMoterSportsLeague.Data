86b1c53588579a149b603435e4893f851f0b89e5
yongsun.park <yongsun.park@penta-cube.com>
2019-02-28 18:42:05 +0900

    CI Test

---
+++ ForzaMoterSportsLeague.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForzaMoterSportsLeague.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
